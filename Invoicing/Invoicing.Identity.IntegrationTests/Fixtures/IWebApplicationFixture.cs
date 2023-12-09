832e3c27552d362e3ca854e27bd32f4522a47536
oreze <kunas1@o2.pl>
2023-12-09 19:55:39 +0100

    test: add IntegrationTests project testing token acquiring as default users

---
+++ Invoicing/Invoicing.Identity.IntegrationTests/Fixtures/IWebApplicationFixture.cs
@@ -0,0 +1,5 @@
+﻿namespace Invoicing.Identity.Tests.Fixtures;
+
+public class IWebApplicationFixture
+{
+}
---
+++ Invoicing/Invoicing.Identity.IntegrationTests/Helpers/HttpClientHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Invoicing.Identity.Tests.Helpers;
+
+public static class HttpClientHelper
+{
+    private static readonly JsonSerializerOptions _defaultSerializerOptions;
+
+    static HttpClientHelper()
+    {
+        _defaultSerializerOptions = new JsonSerializerOptions
+        {
+            WriteIndented = true,
+            Converters =
+            {
+                new JsonStringEnumConverter(JsonNamingPolicy.CamelCase)
+            }
+        };
+    }
+
+    public static async Task<T> GetDeserializedResponseAsync<T>(HttpResponseMessage response)
+    {
+        var responseBody = await response.Content.ReadAsStringAsync();
+        return JsonSerializer.Deserialize<T>(responseBody, _defaultSerializerOptions);
+    }
+}
