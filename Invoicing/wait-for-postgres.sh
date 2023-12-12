5756cb54b87e25d1efe417e916caa2f66c7d0d2c
oreze <kunas1@o2.pl>
2023-12-12 18:48:21 +0100

    gifix: wait for postgres container to be ready before running API

---
+++ Invoicing/wait-for-postgres.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# wait-for-postgres.sh
+
+set -e
+
+host="$1"
+shift
+cmd="$@"
+
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
+  >&2 echo 
